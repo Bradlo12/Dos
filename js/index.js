addb293ba6353d426ec487b96d91b7adadc8a306
Bradlo12 <dereek.bradleyy14@gmail.com>
2022-12-13 15:14:42 -0600

    yes

---
+++ js/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const documentReady = () => {
+  const heroTitle = document.querySelector('.hero__title');
+  const heroButtonSi = document.getElementById('heroButtonSi');
+  const heroButtonNo = document.querySelector('#heroButtonNo');
+
+  const nowZomosNobios = () => {
+    alert('Esoooooo');
+    alert('Siuuuu t hare feli');
+    location.href = 'https://www.youtube.com/watch?v=2Zy_gykQHAA';
+  };
+
+  const evitarKCRompanMyBobo = () => {
+    heroButtonNo.style.position = 'absolute';
+    heroButtonNo.style.top = (Math.random() * window.innerHeight) + 'px';
+    heroButtonNo.style.left = (Math.random() * window.innerWidth) + 'px';
+  };
+
+  heroButtonSi.addEventListener('click', nowZomosNobios);
+  heroButtonNo.addEventListener('mouseover', evitarKCRompanMyBobo);
+};
+
+document.addEventListener('DOMContentLoaded', documentReady);
